5fe2dfc1aa13dd251cc79404fd46432d99457c99
woutv <woutvandamme.wvd@gmail.com>
2025-02-05 23:06:27 +0100

    basic player controller

--- main.js
+++ main.js
@@ -4,13 +4,72 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 
-function gameLoop() {
-ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-requestAnimationFrame(gameLoop);
+const input = {
+    left: false,
+    right: false,
+    up: false,
+    down: false,
+}
+
+
+
+const player = new Player(100, 100, 100)
+
+
+function gameLoop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    player.update(input);
+
+
+
+    player.render(ctx);
+
+    requestAnimationFrame(gameLoop);
 }
 
 gameLoop();
 
 
-const player = new Player(0, 0, 0);
+
+
+// Input
+
+document.addEventListener('keydown', (event) => {
+
+    switch (event.key) {
+        case "ArrowLeft":
+            input.left = true;
+            break;
+        case "ArrowRight":
+            input.right = true;
+            break;
+        case "ArrowUp":
+            input.up = true;
+            break;
+        case "ArrowDown":
+            input.down = true;
+            break;
+    }
+});
+
+
+document.addEventListener('keyup', (event) => {
+
+    switch (event.key) {
+        case "ArrowLeft":
+            input.left = false;
+            break;
+        case "ArrowRight":
+            input.right = false;
+            break;
+        case "ArrowUp":
+            input.up = false;
+            break;
+        case "ArrowDown":
+            input.down = false;
+            break;
+    }
+});
--- model/player.js
+++ model/player.js
@@ -1,8 +1,46 @@
 class Player {
+
+    SPEED = 1;
+    WIDTH = 32;
+    HEIGHT = 32;
+
     constructor(x, y, maxHP) {
         this.x = x;
         this.y = y;
         this.maxHP = maxHP;
+        this.hp = maxHP;
+    }
+
+
+    update(input) {
+
+
+        let addX = 0.0;
+        let addY = 0.0;
+
+        if (input.left) {
+            addX -= this.SPEED;
+        }
+        if (input.right) {
+            addX += this.SPEED;
+        }
+        if (input.up) {
+            addY -= this.SPEED;
+        }
+        if (input.down) {
+            addY += this.SPEED;
+        }
+        // Normalize the new position
+        const totalPositionDiff = Math.sqrt(addX ** 2 + addY ** 2);
+        if (totalPositionDiff > 0.0) {
+            this.x += addX * (this.SPEED / totalPositionDiff);
+            this.y += addY * (this.SPEED / totalPositionDiff);
+        }
+    }
+
+    render(ctx) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.x, this.y, this.WIDTH, this.HEIGHT);
     }
 }
 
