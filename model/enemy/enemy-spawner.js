ead0d488d5c36a1a00dbef27b049139dc8f9a204
woutv <woutvandamme.wvd@gmail.com>
2025-02-21 19:46:51 +0100

    fix spawner

--- model/enemy/enemy-spawner.js
+++ model/enemy/enemy-spawner.js
@@ -17,9 +17,6 @@ class EnemySpawner {
             return Math.random() * (max - min) + min;
         }
 
-        const centerX = this.game.camera.x + this.game.camera.width / 2;
-        const centerY = this.game.camera.y + this.game.camera.height / 2;
-
         // furthest
         const size = Math.sqrt((this.game.camera.width)**2 + (this.game.camera.height)**2) / 2;
 
@@ -30,9 +27,9 @@ class EnemySpawner {
 
             const angle = random(0, 360);
 
-            const x = centerX + (((maxDistance - minDistance) * Math.random() + minDistance) * Math.cos(angle));
-            const y = centerY + (((maxDistance - minDistance) * Math.random() + minDistance) * Math.sin(angle));
-
+            const x = this.game.player.x + (((maxDistance - minDistance) * Math.random() + minDistance) * Math.cos(angle));
+            const y = this.game.player.y + (((maxDistance - minDistance) * Math.random() + minDistance) * Math.sin(angle));
+            
             EntityUtil.addToEntityList(new Enemy(x, y, 1, this.game), this.game.entityList);
         }
     }
