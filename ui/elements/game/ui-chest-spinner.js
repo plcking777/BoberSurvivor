bc71dd067bac29429aa800b6d3e806b7d0c6a2a9
Wout Van Damme <woutvandamme.wv@gmail.com>
2025-03-01 18:36:19 +0100

    better item scrolling right

--- ui/elements/game/ui-chest-spinner.js
+++ ui/elements/game/ui-chest-spinner.js
@@ -30,18 +30,22 @@ class UIChestSpinner extends UIElement {
         ctx.fillRect(this.x, this.y, this.width, this.height);
 
 
-        for (let i = 0; i < this.VISIBLE_SLOTS; i++) {
-            const xPos = this.x + this.spinOffset + 96*i;
+        for (let i = 0; i < this.VISIBLE_SLOTS + 1; i++) {
+            const xPos = this.x + this.spinOffset + 96*i - 96;
             const index = (this.spinIndex + i) % this.weaponImages.length;
-            ctx.drawImage(this.assetHandler.getImage(this.weaponImages[index]), xPos, this.y, 64, 64);
+            //ctx.drawImage(this.assetHandler.getImage(this.weaponImages[index]), xPos, this.y, 64, 64);
             
+            const cropX = Math.max(Math.min(this.x + this.width - xPos, 64), 0);
+            ctx.drawImage(
+                this.assetHandler.getImage(this.weaponImages[index]), 
+                0, 0, cropX, 64,
+                xPos, this.y, cropX, 64
+              );
 
-            if (i >= this.VISIBLE_SLOTS-1) {
-                console.log('xPos: ', xPos, '  >  ', this.width);
-            }
-            
+
+
             if (i >= this.VISIBLE_SLOTS-1 && xPos >= this.x + this.width) {
-                this.spinIndex += 3;
+                this.spinIndex += this.VISIBLE_SLOTS + 1;
                 this.spinOffset = 0;
             }
         }
